c4136197ac8837efeea96e493ac283f8bba844d3
jmoo <jamie777@mail.com>
2014-11-12 19:03:44 -0800

    red-first red ! no pwd validator class

---
+++ JunitBook/src/com/practicalunittesting/chp04/PasswordValidatorTest.java
@@ -0,0 +1,12 @@
+package com.practicalunittesting.chp04;
+
+import org.junit.Test;
+
+public class PasswordValidatorTest {
+
+	@Test
+	public void must_check_password_min_size() {
+		PasswordValidator pv = new PasswordValidator();
+	}
+
+}
